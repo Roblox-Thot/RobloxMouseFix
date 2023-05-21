5a38d478d781a7ca13a16476b146c5f0a20b86b6
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-05-21 00:55:22 -0400

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -13,14 +13,37 @@ else:
 # I don`t see a reason to import unless everything is ready
 from win32gui import GetWindowText, GetForegroundWindow
 from win32.win32api import GetKeyState
+from time import sleep
 import pyautogui
 
+first_toggle = False
+
+def first_check(first_toggle): # This is not the best way to do it but i don't care rn
+    if (GetKeyState(0xDC) < 0):
+        while (GetKeyState(0xDC) < 0):
+            pass
+        return not first_toggle
+    else:
+        return first_toggle
+
+print("\nPress \\ To toggle First person\nStatus:")
+
 while True:
-    window_x, window_y, window_width, window_height = window.left, window.top, window.width, window.height
+    window_center_x = window.left + (window.width // 2)
+    window_center_y = window.top + (window.height // 2)
     curr_window_title = GetWindowText(GetForegroundWindow())
 
     mouse_x, mouse_y = pyautogui.position()
 
-    while (GetKeyState(0x02) < 0):
+    print(f'First Person lock: {first_toggle}          ',end='\r')
+    while (GetKeyState(0x02) < 0) or first_toggle:
         if (curr_window_title == window_title):
-            pyautogui.moveTo(mouse_x, mouse_y, duration=0)
+            if first_toggle:
+                pyautogui.moveTo(window_center_x, window_center_y, duration=0)
+            else:
+                pyautogui.moveTo(mouse_x, mouse_y, duration=0)
+        first_toggle = first_check(first_toggle)
+    
+    first_toggle = first_check(first_toggle)
+
+    sleep(0.01)
